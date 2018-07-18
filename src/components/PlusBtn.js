2e6358c54500d88c4276b165622e0c5151167d25
yutaroyutaro <yutaroyutaro172@gmail.com>
2018-07-18 21:53:01 +0900

    first commit

---
+++ src/components/PlusBtn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NumBtn from "./NumBtn";
+
+const PlusBtn = () => (
+  <button>{n}</button>
+);
+
+export default NumBtn;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CalculatorContainer from './containers/CalculatorContainer';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <CalculatorContainer />,
+    document.getElementById('root')
+);
+registerServiceWorker();
